c4351b0bd47a6baf59741430b64196efeeb94076
Kai (Luyen) Nguyen <VNGUYEN@maxis.com.my>
2024-02-01 10:39:49 +0700

    [tag]staging

--- cicd/deploy.sh
+++ cicd/deploy.sh
@@ -7,6 +7,8 @@ sed -i 's|IMAGE-TAG|'$tag'|' docker-compose.yaml
 sed -i 's|P-MAPPING|'$port_mapping'|' docker-compose.yaml
 sed -i 's|MOUNT-DATA-FOLDER|'$mount_data_folder'|' docker-compose.yaml
 echo "$CI_REGISTRY_PW" | sudo docker login registry.gitlab.com -u "$CI_REGISTRY_USER" --password-stdin
+sudo docker container prune -f
+sudo docker image prune -a -f
 sudo docker tag $image_name:$tag  $image_name:$img_bak_tag
 sudo docker-compose pull
 sudo docker-compose --project-name $project_name down
