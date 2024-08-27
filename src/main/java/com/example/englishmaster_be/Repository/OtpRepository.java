40edc259becf77c2ffc5f0ee4563e821484484de
LaHongVi <vixhoongla@gmail.com>
2024-08-27 12:45:03 +0700

    fix future ME9

---
+++ src/main/java/com/example/englishmaster_be/Repository/OtpRepository.java
@@ -0,0 +1,10 @@
+package com.example.englishmaster_be.Repository;
+
+import com.example.englishmaster_be.Model.Otp;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OtpRepository extends JpaRepository<Otp,String> {
+
+}
